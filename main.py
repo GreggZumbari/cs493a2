209154460cc28403b43c3084d8c53901b8c75dbb
Gregg H. Windows 10 <greggory.png@gmail.com>
2025-04-22 20:04:39 -0700

    Complete (with small errors)

--- main.py
+++ main.py
@@ -19,8 +19,22 @@ def create_business():
 
     # If all fields are there, return 201 Created, otherwise 400 Bad Request
     if data and owner_id and name and street_address and city and state and zip_code:
-        store_business(owner_id, name, street_address, city, state, zip_code)
-        return jsonify({"status": "success"}), 201
+        id = store_business(owner_id, name, street_address, city, state, zip_code)
+        # 201 Created
+        return (
+            jsonify({
+                "id": id,
+                "owner_id": owner_id,
+                "name": name,
+                "street_address": street_address,
+                "city": city,
+                "state": state,
+                "zip_code": zip_code
+            }),
+            201,
+            {"Content-Type": "application/json"}
+        )
+    # 400 Bad Request
     return (
         jsonify({
             "Error": "The request body is missing at least one of the required attributes"
@@ -30,7 +44,7 @@ def create_business():
     )
 
 def store_business(owner_id, name, street_address, city, state, zip_code):
-    entity = datastore.Entity(key=datastore_client.key("owner_id"))
+    entity = datastore.Entity(key=datastore_client.key("Businesses"))
     entity.update({
         "owner_id": owner_id,
         "name": name,
@@ -39,44 +53,332 @@ def store_business(owner_id, name, street_address, city, state, zip_code):
         "state": state,
         "zip_code": zip_code
     })
-
     datastore_client.put(entity)
 
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key("visit"))
-    entity.update({"timestamp": dt})
+    return entity.key.id
 
+@app.route("/businesses/<int:business_id>", methods=["GET"])
+def fetch_business_by_id(business_id):
+    # Fetch business
+    key = datastore_client.key("Businesses", business_id)
+    business = datastore_client.get(key)
+
+    if business is not None:
+        # Add the id because it doesn't come with it by default
+        business_with_id = dict(business)
+        business_with_id["id"] = business_id
+
+        # 200 OK
+        return (
+            jsonify(business_with_id),
+            200,
+            {"Content-Type": "application/json"}
+        )
+    # 404 Not Found
+    return (
+        jsonify({
+            "Error": "No business with this business_id exists"
+        }),
+        404,
+        {"Content-Type": "application/json"}
+    )
+
+@app.route("/businesses", methods=["GET"])
+def fetch_all_businesses():
+    # Fetch the businesses as an iterator
+    query = datastore_client.query(kind="Businesses")
+    data = query.fetch()
+
+    # Add id to each one
+    businesses = []
+    for business in data:
+        business_with_id = dict(business)
+        business_with_id["id"] = business.key.id
+        businesses.append(business_with_id)
+    
+    # 200 OK
+    return (
+        jsonify(businesses),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+@app.route("/businesses/<int:business_id>", methods=["PUT"])
+def edit_business_by_id(business_id):
+    # Fetch the already existing business
+    key = datastore_client.key("Businesses", business_id)
+    business = datastore_client.get(key)
+
+    if business is None:
+        # 404 Not Found
+        return jsonify({"Error": "No business with this business_id exists"}), 404
+    
+    # Get data from request to update the business with
+    data = request.get_json()
+    owner_id = data.get("owner_id")
+    name = data.get("name")
+    street_address = data.get("street_address")
+    city = data.get("city")
+    state = data.get("state")
+    zip_code = data.get("zip_code")
+
+    # If all fields are there edit the business & return 200 OK, otherwise 400 Bad Request
+    if data and owner_id and name and street_address and city and state and zip_code:
+        # Update business in-place
+        business["owner_id"] = owner_id
+        business["name"] = name
+        business["street_address"] = street_address
+        business["city"] = city
+        business["state"] = state
+        business["zip_code"] = zip_code
+
+        # Save the updated business to datastore
+        datastore_client.put(business)
+        
+        # 200 OK
+        return (
+            jsonify({
+                "id": business_id,
+                "owner_id": owner_id,
+                "name": name,
+                "street_address": street_address,
+                "city": city,
+                "state": state,
+                "zip_code": zip_code
+            }),
+            200,
+            {"Content-Type": "application/json"}
+        )
+    # 400 Bad Request
+    return (
+        jsonify({
+            "Error": "The request body is missing at least one of the required attributes"
+        }),
+        400,
+        {"Content-Type": "application/json"}
+    )
+
+@app.route("/businesses/<int:business_id>", methods=["DELETE"])
+def delete_business_by_id(business_id):
+    # Fetch the already existing business
+    key = datastore_client.key("Businesses", business_id)
+    business = datastore_client.get(key)
+
+    if business is None:
+        # 404 Not Found
+        return jsonify({"Error": "No business with this business_id exists"}), 404
+    
+    # Delete the business by its key
+    datastore_client.delete(key)
+
+    # 204 No Content
+    return "", 204
+
+@app.route("/owners/<int:owner_id>/businesses", methods=["GET"])
+def fetch_business_by_owner(owner_id):
+    # Fetch businesses owned by the owner with owner_id
+    # SQL Equivalent: SELECT * FROM Businesses WHERE owner_id=owner_id
+    query = datastore_client.query(kind="Businesses")
+    query.add_filter("owner_id", "=", owner_id)
+    data = query.fetch()
+
+    # Add id to each one
+    businesses = []
+    for business in data:
+        business_with_id = dict(business)
+        business_with_id["id"] = business.key.id
+        businesses.append(business_with_id)
+
+    # 200 OK
+    return (
+        jsonify(businesses),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+@app.route("/reviews", methods=["POST"])
+def create_review():
+    # Get data
+    data = request.get_json()
+    user_id = data.get("user_id")
+    business_id = data.get("business_id")
+    stars = data.get("stars")
+
+    if data and user_id and business_id and stars:
+        # Make sure the business being reviewed exists
+        # SQL Equivalent: SELECT * FROM Businesses WHERE id=business_id
+        business_key = datastore_client.key("Businesses", int(business_id))
+        business = datastore_client.get(business_key)
+
+        if business is None:
+            # 404 Not Found
+            return (
+                jsonify({
+                    "Error": "No business with this business_id exists"
+                }),
+                404,
+                {"Content-Type": "application/json"}
+            )
+        
+        # Make sure there aren't already any reviews with same user_id & business_id
+        # SQL Equivalent: SELECT * FROM Reviews WHERE user_id=user_id AND business_id=business_id
+        conflict_query = datastore_client.query(kind="Reviews")
+        conflict_query.add_filter("business_id", "=", business_id)
+        conflict_query.add_filter("user_id", "=", user_id)
+        conflict_data = conflict_query.fetch()
+
+        if list(conflict_data):
+            # 409 Conflict
+            return (
+                jsonify({
+                    "Error": "You have already submitted a review for this business. You can update your previous review, or delete it and submit a new review"
+                }),
+                409,
+                {"Content-Type": "application/json"}
+            )
+        
+        # Create the review in datastore
+        id = store_review(data)
+
+        # Add the id because it doesn't come with it by default
+        review_with_id = dict(data)
+        review_with_id["id"] = id
+
+        # 201 Created
+        return (
+            jsonify(review_with_id),
+            201,
+            {"Content-Type": "application/json"}
+        )
+    # 400 Bad Request
+    return (
+        jsonify({
+            "Error": "The request body is missing at least one of the required attributes"
+        }),
+        400,
+        {"Content-Type": "application/json"}
+    )
+
+def store_review(data):
+    entity = datastore.Entity(key=datastore_client.key("Reviews"))
+    entity.update(data)
     datastore_client.put(entity)
 
+    return entity.key.id
 
-def fetch_times(limit):
-    query = datastore_client.query(kind="visit")
-    query.order = ["-timestamp"]
+@app.route("/reviews/<int:review_id>", methods=["GET"])
+def fetch_review_by_id(review_id):
+    # Fetch review
+    key = datastore_client.key("Reviews", review_id)
+    review = datastore_client.get(key)
 
-    times = query.fetch(limit=limit)
+    if review is not None:
+        # Add the id because it doesn't come with it by default
+        review_with_id = dict(review)
+        review_with_id["id"] = review_id
 
-    return times
+        # 200 OK
+        return (
+            jsonify(review_with_id),
+            200,
+            {"Content-Type": "application/json"}
+        )
+    # 404 Not Found
+    return (
+        jsonify({
+            "Error": "No review with this review_id exists"
+        }),
+        404,
+        {"Content-Type": "application/json"}
+    )
 
+@app.route("/reviews/<int:review_id>", methods=["PUT"])
+def edit_review_by_id(review_id):
+    # Fetch the already existing review
+    key = datastore_client.key("Reviews", review_id)
+    review = datastore_client.get(key)
 
-def fetch_names(limit):
-    query = datastore_client.query(kind="name")
-    query.order = ["-timestamp"]
+    if review is None:
+        # 404 Not Found
+        return jsonify({"Error": "No review with this review_id exists"}), 404
+    
+    # Get data from request to update the review with
+    data = request.get_json()
+    user_id = data.get("user_id")
+    business_id = data.get("business_id")
+    stars = data.get("stars")
+    review_text = data.get("review_text")
 
-    names = query.fetch(limit=limit)
+    # If all required fields are there...
+    if data and stars:
+        # Update review in-place
+        review["stars"] = stars
+        if user_id:
+            review["user_id"] = user_id
+        if business_id:
+            review["business_id"] = business_id
+        if review_text:
+            review["review_text"] = review_text
 
-    return names
+        # Save the updated review to datastore
+        datastore_client.put(review)
+        
+        # 200 OK
+        return (
+            review,
+            200,
+            {"Content-Type": "application/json"}
+        )
+    # 400 Bad Request
+    return (
+        jsonify({
+            "Error": "The request body is missing at least one of the required attributes"
+        }),
+        400,
+        {"Content-Type": "application/json"}
+    )
 
+@app.route("/reviews/<int:review_id>", methods=["DELETE"])
+def delete_review_by_id(review_id):
+    # Fetch the already existing review
+    key = datastore_client.key("Reviews", review_id)
+    review = datastore_client.get(key)
+
+    if review is None:
+        # 404 Not Found
+        return jsonify({"Error": "No review with this review_id exists"}), 404
+    
+    # Delete the review by its key
+    datastore_client.delete(key)
+
+    # 204 No Content
+    return "", 204
+
+@app.route("/users/<int:user_id>/reviews", methods=["GET"])
+def fetch_reviews_by_user(user_id):
+    # Fetch reviews owned by the user with user_id
+    # SQL Equivalent: SELECT * FROM Reviews WHERE user_id=user_id
+    query = datastore_client.query(kind="Reviews")
+    query.add_filter("user_id", "=", user_id)
+    data = query.fetch()
+
+    # Add id to each one
+    reviews = []
+    for review in data:
+        review_with_id = dict(review)
+        review_with_id["id"] = review.key.id
+        reviews.append(review_with_id)
+
+    # 200 OK
+    return (
+        jsonify(reviews),
+        200,
+        {"Content-Type": "application/json"}
+    )
 
 @app.route("/")
 def root():
-    # Store the current access time in Datastore.
-    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
-
-    # Fetch the most recent 10 access times and names from Datastore.
-    times = fetch_times(10)
-    names = fetch_names(10)
-
-    return render_template("index.html", times=times, names=names)
+    return render_template("index.html", time=datetime.datetime.now(tz=datetime.timezone.utc))
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
--- static/script.js
+++ static/script.js
@@ -34,6 +34,242 @@ function createBusiness() {
             console.log(response);
         })
         .catch((error) => {
-            console.error("Error submitting name. Code: ", error);
+            console.error("Error creating business. Code:", error);
+        });
+}
+
+function fetchBusinessById(business_id) {
+    fetch(`/businesses/${business_id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Business not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Business data:", data);
+        })
+        .catch((error) => {
+            console.error("Error fetching business. Code:", error);
+        });
+}
+
+function fetchAllBusinesses() {
+    fetch(`/businesses`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Server error. Code:", error);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Business data:", data);
+        })
+        .catch((error) => {
+            console.error("Error fetching businesses. Code:", error);
+        });
+}
+
+function editBusinessById() {
+    const owner_id = document.getElementById("owner_id").value;
+    const name = document.getElementById("name").value;
+    const street_address = document.getElementById("street_address").value;
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+    const zip_code = document.getElementById("zip_code").value;
+    
+    fetch(`/businesses/${business_id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                "id": business_id,
+                "owner_id": owner_id,
+                "name": name,
+                "street_address": street_address,
+                "city": city,
+                "state": state,
+                "zip_code": zip_code
+            }
+        ),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Business not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error("Error fetching or editing business. Code:", error);
+        });
+}
+
+function deleteBusinessById() {
+    fetch(`/businesses/${business_id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Business not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error("Error fetching or deleting business. Code:", error);
+        });
+}
+
+function fetchBusinessByOwner() {
+    fetch(`/owners/${owner_id}/businesses`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Owner not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Owner's business data:", data);
+        })
+        .catch((error) => {
+            console.error("Error fetching owner's businesses. Code:", error);
+        });
+}
+
+function createReview() {
+    const user_id = document.getElementById("user_id").value;
+    const business_id = document.getElementById("business_id").value;
+    const stars = document.getElementById("stars").value;
+    const review_text = document.getElementById("review_text").value;
+
+    fetch("/reviews", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            {
+                "user_id": user_id,
+                "business_id": business_id,
+                "stars": stars,
+                "review_text": review_text
+            }
+        ),
+    })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error("Error creating review. Code:", error);
+        });
+}
+
+function fetchReviewById(review_id) {
+    fetch(`/businesses/${review_id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Review not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Review data:", data);
+        })
+        .catch((error) => {
+            console.error("Error fetching review. Code:", error);
+        });
+}
+
+function editReviewById() {
+    const user_id = document.getElementById("user_id").value;
+    const business_id = document.getElementById("business_id").value;
+    const stars = document.getElementById("stars").value;
+    const review_text = document.getElementById("review_text").value;
+
+    fetch(`/reviews/${reviews_id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                "user_id": user_id,
+                "business_id": business_id,
+                "stars": stars,
+                "review_text": review_text
+            }
+        ),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Review not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error("Error fetching or editing review. Code:", error);
+        });
+}
+
+function deleteReviewById() {
+    fetch(`/reviews/${reviews_id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("Review not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error("Error fetching or deleting review. Code:", error);
+        });
+}
+
+function fetchReviewsByUser() {
+    fetch(`/users/${user_id}/reviews`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.error("User not found or server error. Code:", error);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("User's review data:", data);
+        })
+        .catch((error) => {
+            console.error("Error fetching user's reviews. Code:", error);
         });
 }
